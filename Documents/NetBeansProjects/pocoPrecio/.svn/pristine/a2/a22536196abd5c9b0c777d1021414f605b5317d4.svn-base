cbe687631b2816e263b372606128696b5f862110
maurodim <mauro@bambusoft.com.ar>
2019-05-25 10:32:48 -0300

    alertas de mail enviado

---
+++ Documents/NetBeansProjects/pocoPrecio/.svn/pristine/a2/a22536196abd5c9b0c777d1021414f605b5317d4.svn-base
@@ -0,0 +1,140 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbsgestion;
+
+import Configuracion.Propiedades;
+import interfaceGraficas.LoguinBbsGestion;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import static java.lang.Thread.sleep;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import objetos.ConeccionLocal;
+import pantalla.Ventana;
+
+/**
+ *
+ * @author mauro
+ */
+public class BbsGestion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        /*
+        ArrayList usuariosList=new ArrayList();
+        Usuarios usuarios=new Usuarios();
+        usuariosList=usuarios.listarUsuario();
+        */
+         try{
+           // UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+            //UIManager.setLookAndFeel("com.jtattoo.plaf.aero.AeroLookAndFeel");
+            UIManager.setLookAndFeel("com.jtattoo.plaf.aluminium.AluminiumLookAndFeel");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+         Ventana ventana=new Ventana();
+        ventana.setVisible(true);
+        File folder=new File("C:\\Gestion");
+        File archivos=new File("C:\\Informes");
+        File bases=new File("C:\\Gestion\\DB");
+        //File imagenes=new File("C:\\Gestion\\imagenes\\saynomore.jpg");
+        File bk;
+        //FileInputStream fregis = new FileInputStream("C:\\Users\\mauro\\Pictures\\Camera Uploads\\snm.jpg"); 
+        
+
+        File archivo=null;
+        FileReader fr=null;
+        BufferedReader br=null;
+        if(!bases.isDirectory()){
+            JOptionPane.showMessageDialog(null,"INICIANDO CONFIGURACION Y CREACION DE LA BASE DE DATOS");
+            bases.mkdirs();
+            ConeccionLocal.CrearDb();
+            
+        }
+        if(!folder.isDirectory()){
+            //System.out.println("EXISTE EL DIRECTORIO");
+            folder.mkdirs();
+        }else{
+            //System.out.println("NOOOOOOOOOOOOOOO EXISTE EL DIRECTORIO");
+            
+        }
+        if(!archivos.isDirectory())archivos.mkdirs();
+        /*
+        if(!imagenes.isFile()){
+            //imagenes.mkdirs();
+            FileOutputStream fsalida = new FileOutputStream("C:\\Gestion\\imagenes\\saynomore.jpg", true); 
+        int b = fregis.read(); 
+        while (b != -1) { 
+        fsalida.write(b); 
+        b = fregis.read(); 
+
+        } 
+        
+        fsalida.flush(); 
+        fsalida.close();
+         
+       
+            
+        }
+          
+        fregis.close();
+        */
+        bk=new File("C:\\Gestion\\backUp.sql");
+        //String sql="select * from movimientoscaja into outfile "+bk+" FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n\r'";
+       // Transaccionable tra=new Conecciones();
+        //tra.guardarRegistro(sql);
+        
+        try {
+         // Apertura del fichero y creacion de BufferedReader para poder
+         // hacer una lectura comoda (disponer del metodo readLine()).
+         archivo = new File ("C:\\Gestion\\erroresDeConeccion.txt");
+         if(archivo.exists()){
+         fr = new FileReader (archivo);
+         br = new BufferedReader(fr);
+ 
+         // Lectura del fichero
+         String linea;
+          //Transaccionable tra=new Conecciones();
+         while((linea=br.readLine())!=null){
+             
+            //System.out.println(linea);
+           
+           // if(tra.guardarRegistro(linea));
+      }
+        }
+        }
+      catch(Exception e){
+         e.printStackTrace();
+      }finally{
+         // En el finally cerramos el fichero, para asegurarnos
+         // que se cierra tanto si todo va bien como si salta 
+         // una excepcion.
+         try{                    
+            if( null != fr ){   
+               fr.close();     
+            }                  
+         }catch (Exception e2){ 
+            e2.printStackTrace();
+         }
+      }
+        
+         Propiedades.CargarPropiedades();
+        try {
+            sleep(3000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(BbsGestion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        LoguinBbsGestion lBb=new LoguinBbsGestion();
+        lBb.setVisible(true);
+        lBb.pack();
+    }
+}
---
+++ Documents/NetBeansProjects/pocoPrecio/src/objetos/Mail.java
@@ -0,0 +1,118 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetos;
+
+import Configuracion.Propiedades;
+import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Mail {
+    private final Properties propiedades;
+    private String password;
+    private Session sesion;
+    private String direccionFile;
+    private String detalleListado;
+    private String asunto;
+    private String correoSaliente;
+    private String copiaCorreo;
+
+    
+
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    
+    public void setDetalleListado(String detalleListado) {
+        this.detalleListado = detalleListado;
+    }
+    
+    
+    public void setDireccionFile(String direccionFile) {
+        this.direccionFile = direccionFile;
+    }
+    
+    public Mail() {
+        propiedades=new Properties();
+        password=Propiedades.getPASS();
+        correoSaliente=Propiedades.getCORREOSALIENTE();
+        copiaCorreo=Propiedades.getCORREOCC();
+        
+    }
+    private void init(){
+        propiedades.put("mail.smtp.host","mail.bambusoft.com.ar");
+        propiedades.put("mail.smtp.starttls.enable","false");
+        propiedades.put("mail.smtp.port",587);
+        propiedades.put("mail.smtp.mail.sender",correoSaliente);
+        propiedades.put("mail.smtp.user",correoSaliente);
+        propiedades.put("mail.smtp.auth","true");
+        sesion=Session.getDefaultInstance(propiedades);
+        
+    }
+    public void enviarMailRepartoCargaCompleta() throws MessagingException{
+        init();
+        try{
+            MimeMessage mensaje=new MimeMessage(sesion);
+            mensaje.setFrom(new InternetAddress((String)propiedades.get("mail.smtp.mail.sender")));
+            mensaje.addRecipient(Message.RecipientType.TO,new InternetAddress("alexlebbo@hotmail.com"));
+            mensaje.addRecipient(Message.RecipientType.CC,new InternetAddress(copiaCorreo));
+            mensaje.addRecipient(Message.RecipientType.BCC,new InternetAddress("mauro@bambusoft.com.ar"));
+            System.out.println("DATOS DEL CORREO SALIENTE "+correoSaliente+" // "+password);
+            mensaje.setSubject(asunto);
+            BodyPart texto=new MimeBodyPart();
+            texto.setText("INFORME GENERADO POR CIERRE DE CAJA   \n Saludos");
+            BodyPart adjunto=new MimeBodyPart();
+            adjunto.setDataHandler(new DataHandler(new FileDataSource(direccionFile)));
+            adjunto.setFileName(detalleListado);
+            MimeMultipart multiParte=new MimeMultipart();
+            multiParte.addBodyPart(texto);
+            multiParte.addBodyPart(adjunto);
+            //mensaje.setText("El reparto del vehiculo esta cerrado para el reparto. Motivo :CAPACIDAD DE CARGA COMPLETADA");
+            mensaje.setContent(multiParte);
+            Transport t=sesion.getTransport("smtp");
+            t.connect((String)propiedades.get("mail.smtp.user"), password);
+            t.sendMessage(mensaje,mensaje.getAllRecipients());
+            t.close();
+        }catch(MessagingException me){
+            System.err.println("EL MENSAJE NO SE PUDO ENVIAR "+me);
+            JOptionPane.showMessageDialog(null, "EL MENSAJE NO SE PUDO ENVIAR "+me, "FALLO EN MAIL", JOptionPane.WARNING_MESSAGE);
+            //JOptionPane.showMessageDialog(null, "EL MENSAJE NO SE PUDO ENVIAR "+me);
+        }
+        JOptionPane.showMessageDialog(null, "MAIL ENVIADO............. ");
+        
+    }
+   public void enviarMailRepartoCerrado(String descripcionVehiculo,String fecha) throws MessagingException{
+        init();
+        try{
+            MimeMessage mensaje=new MimeMessage(sesion);
+            mensaje.setFrom(new InternetAddress((String)propiedades.get("mail.smtp.mail.sender")));
+            mensaje.addRecipient(Message.RecipientType.TO,new InternetAddress("hernangonzalez@sidercon.com"));
+            mensaje.addRecipient(Message.RecipientType.CC,new InternetAddress("rgonzalez@sidercon.com"));
+            mensaje.setSubject("REPARTO CERRADO");
+            mensaje.setText("El reparto del vehiculo "+descripcionVehiculo+" esta cerrado para el reparto del dia "+fecha+". Motivo :CAPACIDAD DE REPARTO COMPLETA");
+            Transport t=sesion.getTransport("smtp");
+            t.connect((String)propiedades.get("mail.smtp.user"), password);
+            t.sendMessage(mensaje,mensaje.getAllRecipients());
+            t.close();
+        }catch(MessagingException me){
+            System.err.println("EL MENSAJE NO SE PUDO ENVIAR "+me);
+        }
+    }
+}
